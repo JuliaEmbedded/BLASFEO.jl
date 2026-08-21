5f007562853ee5d58756f988389a86a0fdbfbe78
Anton Pozharskiy <apozharski@gmail.com>
2026-08-21 22:51:35 +0200

    triangular matrices

--- src/level2.jl
+++ src/level2.jl
@@ -13,6 +13,10 @@ for (Mat, Vec, flag) in [
     blasfeo_gemm_tt = Symbol(:blasfeo_, flag, :gemm_tt)
     blasfeo_symv_l = Symbol(:blasfeo_, flag, :symv_l)
     blasfeo_symv_u = Symbol(:blasfeo_, flag, :symv_u)
+    blasfeo_trmv_lnn = Symbol(:blasfeo_, flag, :trmv_lnn)
+    blasfeo_trmv_lnu = Symbol(:blasfeo_, flag, :trmv_lnu)
+    blasfeo_trmv_unn = Symbol(:blasfeo_, flag, :trmv_unn)
+    blasfeo_trmv_unu = Symbol(:blasfeo_, flag, :trmv_unu)
 
     @eval function Base.:*(A::$Mat, x::$Vec)
         @boundscheck begin
@@ -100,7 +104,7 @@ for (Mat, Vec, flag) in [
                 z, 0,
             )
         end
-        return z # A^T*x
+        return z
     end
 
     @eval function LinearAlgebra.mul!(Y::$Vec, A::Symmetric{eltype($Mat), $Mat}, B::$Vec)
@@ -129,5 +133,132 @@ for (Mat, Vec, flag) in [
         return Y
     end
 
-    
+
+    @eval function Base.:*(A::LowerTriangular{eltype($Mat), $Mat}, x::$Vec)
+        @boundscheck begin
+            size(A,1) == length(x) || throw(DimensionMismatch("Matrix second dimension doesn't match vector dimension"))
+        end
+
+        z = similar(x)
+        $blasfeo_trmv_lnn(
+            size(A,1),
+            1.0, A.data, 0, 0,
+            x, 0,
+            0.0, x, 0,
+            z, 0,
+        )
+        return z # A^T*x
+    end
+
+    @eval function LinearAlgebra.mul!(Y::$Vec, A::LowerTriangular{eltype($Mat), $Mat}, B::$Vec)
+        @boundscheck begin
+            size(A,2) == length(B) || throw(DimensionMismatch("Matrix second dimension doesn't match vector dimension"))
+            size(A,1) == length(Y) || throw(DimensionMismatch("Matrix first dimension doesn't match output vector dimension"))
+        end
+
+        $blasfeo_trmv_lnn(
+            size(A,1),
+            1.0, A.data, 0, 0,
+            B, 0,
+            0.0, B, 0,
+            Y, 0,
+        )
+        return Y
+    end
+
+    @eval function Base.:*(A::UpperTriangular{eltype($Mat), $Mat}, x::$Vec)
+        @boundscheck begin
+            size(A,1) == length(x) || throw(DimensionMismatch("Matrix second dimension doesn't match vector dimension"))
+        end
+
+        z = similar(x)
+        $blasfeo_trmv_unn(
+            size(A,1),
+            1.0, A.data, 0, 0,
+            x, 0,
+            0.0, x, 0,
+            z, 0,
+        )
+        return z # A^T*x
+    end
+
+    @eval function LinearAlgebra.mul!(Y::$Vec, A::UpperTriangular{eltype($Mat), $Mat}, B::$Vec)
+        @boundscheck begin
+            size(A,2) == length(B) || throw(DimensionMismatch("Matrix second dimension doesn't match vector dimension"))
+            size(A,1) == length(Y) || throw(DimensionMismatch("Matrix first dimension doesn't match output vector dimension"))
+        end
+
+        $blasfeo_trmv_unn(
+            size(A,1),
+            1.0, A.data, 0, 0,
+            B, 0,
+            0.0, B, 0,
+            Y, 0,
+        )
+        return Y
+    end
+
+    @eval function Base.:*(A::UnitLowerTriangular{eltype($Mat), $Mat}, x::$Vec)
+        @boundscheck begin
+            size(A,1) == length(x) || throw(DimensionMismatch("Matrix second dimension doesn't match vector dimension"))
+        end
+
+        z = similar(x)
+        $blasfeo_trmv_lnu(
+            size(A,1),
+            1.0, A.data, 0, 0,
+            x, 0,
+            0.0, x, 0,
+            z, 0,
+        )
+        return z # A^T*x
+    end
+
+    @eval function LinearAlgebra.mul!(Y::$Vec, A::UnitLowerTriangular{eltype($Mat), $Mat}, B::$Vec)
+        @boundscheck begin
+            size(A,2) == length(B) || throw(DimensionMismatch("Matrix second dimension doesn't match vector dimension"))
+            size(A,1) == length(Y) || throw(DimensionMismatch("Matrix first dimension doesn't match output vector dimension"))
+        end
+
+        $blasfeo_trmv_lnu(
+            size(A,1),
+            1.0, A.data, 0, 0,
+            B, 0,
+            0.0, B, 0,
+            Y, 0,
+        )
+        return Y
+    end
+
+    @eval function Base.:*(A::UnitUpperTriangular{eltype($Mat), $Mat}, x::$Vec)
+        @boundscheck begin
+            size(A,1) == length(x) || throw(DimensionMismatch("Matrix second dimension doesn't match vector dimension"))
+        end
+
+        z = similar(x)
+        $blasfeo_trmv_unu(
+            size(A,1),
+            1.0, A.data, 0, 0,
+            x, 0,
+            0.0, x, 0,
+            z, 0,
+        )
+        return z # A^T*x
+    end
+
+    @eval function LinearAlgebra.mul!(Y::$Vec, A::UnitUpperTriangular{eltype($Mat), $Mat}, B::$Vec)
+        @boundscheck begin
+            size(A,2) == length(B) || throw(DimensionMismatch("Matrix second dimension doesn't match vector dimension"))
+            size(A,1) == length(Y) || throw(DimensionMismatch("Matrix first dimension doesn't match output vector dimension"))
+        end
+
+        $blasfeo_trmv_unu(
+            size(A,1),
+            1.0, A.data, 0, 0,
+            B, 0,
+            0.0, B, 0,
+            Y, 0,
+        )
+        return Y
+    end
 end
